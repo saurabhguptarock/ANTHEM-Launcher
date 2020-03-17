50630331ea60c9ed3f1248acacf19bc073bcbd0f
saurabh gupta <saurabhguptarock119@gmail.com>
2020-03-17 17:37:35 +0530

    finalized the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ElectronService } from "./core/services";
 import { TranslateService } from "@ngx-translate/core";
 import { AppConfig } from "../environments/environment";
-import { remote } from "electron";
+import { remote, shell } from "electron";
 
 @Component({
   selector: "app-root",
@@ -10,7 +10,8 @@ import { remote } from "electron";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  public btn_status: string = "INSTALL";
+  public btn_status: string = "START";
+  public game_status: string = "All Files Look Good";
 
   constructor(
     public electronService: ElectronService,
@@ -29,9 +30,22 @@ export class AppComponent {
     }
   }
   minimize_window() {
-    remote.BrowserWindow.getFocusedWindow().minimize();
+    remote.getCurrentWindow().minimize();
   }
   close_window() {
     remote.getCurrentWindow().close();
+  }
+  change_btn_status() {
+    this.btn_status = "RUNNING...";
+    setTimeout(() => {
+      this.btn_status = "START";
+    }, 5000);
+    shell.openItem("Game/ANTHEM.exe");
+  }
+  launch_social_media(launch_url: string) {
+    if (launch_url == "website") shell.openExternal("https://saverl.com");
+    else if (launch_url == "youtube")
+      shell.openExternal("https://youtube.com/saverl");
+    else shell.openExternal("https://facebook.com/saverl");
   }
 }
